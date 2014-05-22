6319de82f33700df238a4bae901437f1c967d974
ByteBlast <byteblast@gmail.com>
2014-05-22 02:28:09 +0100

    added web agent.

---
+++ src/Framework/Common/Extensions.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Framework.Common
+{
+    public static class StreamExtensions
+    {
+        public static string ReadAsString(this Stream stream)
+        {
+            // TODO:
+            //  Consideration for encoding.
+
+            using (var streamReader = new StreamReader(stream))
+                return streamReader.ReadToEnd();
+        }
+    }
+
+    public static class StringExtensions
+    {
+        public static string ParseBetween(this string input, string leftWord, string rightWord)
+        {
+            var pattern = new Regex(leftWord + "(?<word>.*?)" + rightWord);
+            return pattern.Match(input).Groups["word"].Value;
+        }
+    }
+}
