c07e80ba5bfd1807643302d086ea2111bd7117de
ByteBlast <byteblast@gmail.com>
2014-05-23 06:31:52 +0100

    exposed profile service (oops).

--- src/Framework/XboxClient.cs
+++ src/Framework/XboxClient.cs
@@ -6,24 +6,25 @@ namespace Framework
     public class XboxClient
     {
         public XboxClient()
-            : this (Credentials.Annonymous)
+            : this(Credentials.Annonymous)
         {
-                
         }
 
         public XboxClient(Credentials credentials)
         {
             Connection connection = new Connection(credentials);
 
-            Search = new SearchClient(connection);
+            Profile = new ProfileClient(connection);
             Friends = new FriendsClient(connection);
             Games = new GamesClient(connection);
             Achievements = new AchievementsClient(connection);
+            Search = new SearchClient(connection);
         }
 
-        public SearchClient Search { get; private set; }
+        public ProfileClient Profile { get; private set; }
         public FriendsClient Friends { get; private set; }
         public GamesClient Games { get; private set; }
         public AchievementsClient Achievements { get; private set; }
+        public SearchClient Search { get; private set; }
     }
 }
