516ef8a79be42fb1a39ca5bf72161d753a0cfa61
ByteBlast <byteblast@gmail.com>
2014-06-10 01:53:18 +0100

    meaningful error messages for 400 & 500 errors.

---
+++ src/Service/Error.cs
@@ -0,0 +1,8 @@
+﻿namespace Service
+{
+    public class Error
+    {
+        public int StatusCode { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
---
+++ src/Service/ErrorStatusCodeHandler.cs
@@ -0,0 +1,39 @@
+﻿using Nancy;
+using Nancy.ErrorHandling;
+using Nancy.Responses;
+using System.Linq;
+
+namespace Service
+{
+    public class ErrorStatusCodeHandler : IStatusCodeHandler
+    {
+        public bool HandlesStatusCode(HttpStatusCode statusCode, NancyContext context)
+        {
+            return statusCode == HttpStatusCode.NotFound || 
+                   statusCode == HttpStatusCode.InternalServerError;
+        }
+
+        public void Handle(HttpStatusCode statusCode, NancyContext context)
+        {
+            var error = new Error { StatusCode = (int) statusCode };
+            error.ErrorMessage = 
+                statusCode == HttpStatusCode.NotFound ? "Not Found" : "Internal Server Error.";
+
+            // unfavourable approach to content-negotation until I can figure out how to use the
+            // IResponseNegotiator introduced in Nancy 0.23
+
+            var clientWantsXml = context.Request.Headers.Accept.Any(
+                header => header.Item1 == "text/xml" || header.Item1 == "application/xml");
+
+            if (clientWantsXml)
+            {
+                var xmlResponse = new XmlResponse<Error>(error, "text/xml", new DefaultXmlSerializer());
+                context.Response = xmlResponse.WithStatusCode(statusCode);
+                return;
+            }
+
+            var jsonResponse = new JsonResponse(error, new JsonNetSerializer());
+            context.Response = jsonResponse.WithStatusCode(statusCode);
+        }
+    }
+}
